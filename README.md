4ec930d10ac7c4f23f34378cb06b12107ecf32d7
Ryan Freese <ryan.p.freese@gamil.com>
2020-09-24 22:33:57 -0500

    More edits to JavaScript file

---
+++ README.md
@@ -0,0 +1,23 @@
+# Random Password Generator:
+The intention of this application is to generate a random password based on user picked elements.
+
+See here:
+
+[Password Generator]()
+<br>
+[GitHub Repository](https://github.com/KeepTheLidOnTight/Responsive_Portfolio)
+
+# Usage Instructions
+Click on the links in the navbar to view different pages of the site. Resize the browser window to see the responsive layout.
+
+# Installation:
+N/A
+
+# Credits/Thanks:
+Bootstrap was used to design this responsive layout. Google fonts also referenced.
+
+# License Information: 
+N/A
+
+# Furthur Developments/Edits
+-Continue to customize elements of the portfolio
--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
-var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var spec = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 // Write password to the #password input
 function writePassword() {
- 
-  var passwordLength = prompt("How many characters would you like your password to be?")
-  passwordLength = Number(passwordLength)
-  if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
-    alert("Password Invalid!")
-    return;
-  } 
-  var lowerConfirmed = confirm("Lowercase?") 
-  var upperConfirmed = confirm("Uppercase?")
-  var numeric = confirm("Numeric?")
-  var specialCharacters = confirm("Special characters?")
-  if (!lowerConfirmed && !upperConfirmed && !numeric && !specialCharacters){
-    alert("Password Invalid")
-    return;
-    }    
-  
-  
- var password = generatePassword(passwordLength, lowerConfirmed, upperConfirmed, numeric, specialCharacters);
-  var passwordText = document.querySelector("#password");
+    
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
-
-function generatePassword(passwordLength, lowerConfirmed, upperConfirmed, numeric, specialCharacters) {
-  var userArray = []
-  if (lowerConfirmed){
-    userArray = userArray.concat(lettersLower)
     
-  }
-  if (upperConfirmed){
-    userArray = userArray.concat(lettersUpper)
-  }
-  for (var i = 0; i < passwordLength; i++) {
-  
-    var index = Math.floor(Math.random() * userArray.length) + 1;
-
-    console.log(userArray[index])
-    
-  }
-
-  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//////////////////////////////////////////////////////////////
+
+
+
+
+function generatePassword() {
+  // Arrays of characters to be used in password
+var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var spec = ['~','!','@','#','$','%','&','-','+','=','_'];
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// empty containers to hold user choices and generated password
+var userArray = [];
+var genPassword = "";
+
+//*** grab the password length */
+
+passwordLength = prompt("How many characters would you like your password to be?");
+
+  if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
+    alert("Password Invalid (must be between 8 and 128 characters!");
+    return;
+  } else {userArray.push(passwordLength)}
+
+//** */
+
+  var lowerConfirmed = confirm("Lowercase?");
+  var upperConfirmed = confirm("Uppercase?");
+  var numeric = confirm("Numeric?");
+  var specialCharacters = confirm("Special characters?");
+
+  if (!lowerConfirmed && !upperConfirmed && !numeric && !specialCharacters){
+    alert("Password Invalid");
+    return;
+    } else {userArray.push(lettersLower, lettersUpper, spec, lettersUpper)} 
+
+
+    for (var i = 0; i < passwordLength; i++) {
+
+      var index =[Math.floor(Math.random() * passwordLength.length)];
+      genPassword += userArray[index];
+      console.log(password);
+    }
+    return genPassword;
+
+
+}
+
